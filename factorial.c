f32b81430ed856d18a1acc214da6a8ae045e0549
smrn001 <samir.niroula001@gmail.com>
2024-01-28 21:27:14 +0545

    factorial

--- factorial.c
+++ factorial.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main() {
-  int a, i, multiply = 1;
+  int a, i, fact = 1;
   printf("Enter a number \n");
   scanf("%d", &a);
   for (i = 1; i <= a; i++) {
-    multiply = multiply * i;
+    fact = fact * i;
   }
-  printf("Factorial of %d is %d ", a, multiply);
+  printf("Factorial of %d is %d ", a, fact);
   return 0;
 }
