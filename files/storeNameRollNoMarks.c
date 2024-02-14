fa4ca0ada30bba849ae9784235898b0ea14f7eb7
smrn001 <samir.niroula001@gmail.com>
2024-02-14 12:03:34 +0545

    files in c practice

---
+++ files/storeNameRollNoMarks.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+  char name[15];
+  int rollNo;
+  float marks;
+  printf("Enter name :");
+  fgets(name, 15, stdin);
+  printf("Enter Roll No. :");
+  scanf("%d", &rollNo);
+  printf("Enter Marks :");
+  scanf("%f", &marks);
+  FILE *da;
+  da = fopen("student.txt", "a");
+  printf("Name:%s  RollNo:%d \t  Marks:%.2f", name, rollNo, marks);
+  fprintf(da, "\n Name:%s  RollNo:%d \t  Marks:%.2f", name, rollNo, marks);
+  fclose(da);
+  return 0;
+}
