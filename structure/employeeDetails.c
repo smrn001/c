ab1c4de79bdf209144467f9073422f4b8687363b
smrn001 <samir.niroula001@gmail.com>
2024-03-18 15:09:54 +0545

    employee datas ...

---
+++ structure/employeeDetails.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+struct employee {
+  char name[30];
+  int id;
+  float salary;
+};
+
+int main() {
+  struct employee emp[100];
+  int i = 0;
+  char choice;
+  do {
+    printf("Enter name, ID, and salary of employee %d: ", i + 1);
+    scanf("%s %d %f", emp[i].name, &emp[i].id, &emp[i].salary);
+    printf("Do you want to enter data of next employee? (y/n): ");
+    scanf(" %c", &choice);
+    i++;
+  } while (choice == 'y' || choice == 'Y');
+
+  printf("\nEntered employee details:\n");
+  printf("Name \tID \tSalary\n");
+  for (int j = 0; j < i; j++) {
+    printf("%s\t %d\t %.2f\n", emp[j].name, emp[j].id, emp[j].salary);
+  }
+
+  return 0;
+}
