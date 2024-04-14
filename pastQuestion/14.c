aca8dbf70e9c0db6133bc57be6cf1ab59f1ca9f2
smrn001 <samir.niroula001@gmail.com>
2024-04-14 10:32:59 +0545

    past question

--- pastQuestion/14.c
+++ pastQuestion/14.c
@@ -1,30 +1,31 @@
-// find second largest element in an array
-
+/// seconf largest element in an array
 #include <stdio.h>
-int main(){
+
+int main()
+{
     int n;
     printf("Enter the size of the array: ");
     scanf("%d", &n);
+
     int arr[n];
     printf("Enter the elements of the array: ");
-    for(int i=0; i<n; i++){
+    for (int i = 0; i < n; i++)
+    {
         scanf("%d", &arr[i]);
     }
-    int max1 = arr[0], max2 = -1;
-    for(int i=1; i<n; i++){
-        if(arr[i] > max1){
+
+    int max1 = arr[0], max2;
+
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > max1)
+        {
             max2 = max1;
             max1 = arr[i];
         }
-        else if(arr[i] > max2 && arr[i] != max1){
-            max2 = arr[i];
-        }
     }
-    if(max2 == -1){
-        printf("There is no second largest element in the array\n");
-    }
-    else{
-        printf("The second largest element in the array is: %d\n", max2);
-    }
+
+    printf("The second largest element in the array is: %d\n", max2);
+
     return 0;
 }
